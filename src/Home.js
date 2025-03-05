12e4ca767e3926aa4a6d1a51601076f89ef07a3f
SanjanaKote <sanjanakote02@gmail.com>
2025-03-05 16:10:29 +0530

    useState another application like for loop

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,43 @@
 import { useState } from "react";
 
+const Home=()=>{
+    const [blogs,setBlogs]=useState([
+        {title:'My new Website', body:'lorem ispkmk..',author:'mario',id:1},
+        {title:'Welcome Party', body:'lorem ispkmk..',author:'yoshi',id:2},
+    ]);
 
-const Home = () => {
-
-        const [name,setName]=useState('Winter');
-    
-        const handleClick =()=>{
-            setName('Summer')
-    }
-    
-        return (  
-            <div className="home">
-                <p>Hello,{name}</p>
-                <button onClick={handleClick}>Click</button>
+return(
+    <div className="home">
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
             </div>
-        );
-    }
+        ))}
+    </div>
+    );
+}
+ 
+export default Home;
+
+
+
+
+
+
+
+// const Home = () => {
+
+    //     const [name,setName]=useState('Winter');
+    
+    //     const handleClick =()=>{
+    //         setName('Summer')
+    // }
+    
+    //     return (  
+    //         <div className="home">
+    //             <p>Hello,{name}</p>
+    //             <button onClick={handleClick}>Click</button>
+    //         </div>
+    //     );
+    // }

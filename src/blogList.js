dcebdba098776e0bd1d4fa742ef33dddbaf0225c
SanjanaKote <sanjanakote02@gmail.com>
2025-03-05 16:54:20 +0530

    multiple blogs

--- src/blogList.js
+++ src/blogList.js
@@ -1,7 +1,9 @@
 const BlogList = (props) => {
     const blogs=props.blogs;
+    const title=props.title;
     return (
         <div className="blog-BlogList">
+            <h1>{title}</h1>
             {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>

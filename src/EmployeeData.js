324e0d9792eeccd3b4cf222f65d9dd218237542a
SanjanaKote <sanjanakote02@gmail.com>
2025-03-05 15:58:29 +0530

    React for pratice - Basics

---
+++ src/EmployeeData.js
@@ -0,0 +1,14 @@
+const Data=[
+    {
+        id:1,
+        firstName:'Mark',
+        LastName:'David',
+        age:20
+    },
+    {
+        id:2,
+        firstName:'Virat',
+        LastName:'Kholi',
+        age:26
+    },
+]

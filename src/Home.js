bb3582c27a5ec0719186b6b444145586287c061e
SanjanaKote <sanjanakote02@gmail.com>
2025-03-06 16:03:09 +0530

    handle error message

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import BlogList from "./blogList";
 const Home=()=>{
     const [blogs, setBlogs]=useState(null);
     const [isPending,setIsPending]=useState(true);
+    const [error,setError] =useState(null);
+
 
 
 const handleDelete=(id)=>{
@@ -11,20 +13,31 @@ const handleDelete=(id)=>{
     setBlogs(newBlogs);
 }
 
-useEffect(()=>{
-    setTimeout(()=>{
-        fetch('http://localhost:8000/blogs')
-        .then(res=>{
-            return res.json();
-        }).then(data => {
-            setBlogs(data);
-            setIsPending(false);
-        });
-    },1000)
-},[]);
-
+useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error("could not fetch the data for that resource");
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [])
+ 
 return(
     <div className="home">
+        {error && <div>{ error }</div> }
         {isPending && <div>Loading...</div>}
         {blogs && <BlogList  blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
         

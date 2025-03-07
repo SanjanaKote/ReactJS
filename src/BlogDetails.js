2ab3b67299311edc09d6beea36b994d0c5e51c28
SanjanaKote <sanjanakote02@gmail.com>
2025-03-07 12:57:21 +0530

    reusing the custom hook

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,10 +3,19 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} =useParams();
+    const {data:blog,error , isPending}=useFetch('http://localhost:8000/blogs/' +id);
 
     return ( 
         <div className="BlogDetails">
-            <h2>Blog details - {id} </h2>
+            {isPending && <div> Loading</div>}
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
      );
 }
